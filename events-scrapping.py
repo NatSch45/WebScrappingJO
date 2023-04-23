ef6eeff27245b11a02d1946f7a96b6ea18224765
t.bourmaud <tao.bourmaud@ynov.com>
2023-04-23 23:00:28 +0200

    get all events and put them on database

--- events-scrapping.py
+++ events-scrapping.py
@@ -9,9 +9,9 @@ from scrapping import *
 
 #* Constants
 OUTPUT_FILE = './output.json'
-TIME_TO_SLEEP = 1
+TIME_TO_SLEEP = 2
 
-index = 1
+# index = 1
 
 def getEditionsForURL():
 
@@ -48,22 +48,37 @@ def getEvents(edition, sport):
             urlreq.urlopen(req), "lxml"
         )
 
-        eventsDiv = soup.find_all('div', {"class": "styles__RowTitle-sc-282810-1 dvJTpe"})
-
-        
+        eventsDiv = soup.find_all('div', {"class": "styles__RowTitle-sc-282810-1"})
 
         # Pour chaque évenement, on va chercher son nom en minuscule.
         for div in eventsDiv:
+            eventData = {}
             name = div.find("h2").text.lower()
-            global index
-            eventData = {"id": index, "name": name, "sport": sport}
-            index += 1
+            # global index
+            # eventData["id"] = index
+            eventData["name"] = name
+            eventData["sport"] = sport
+            # index += 1
             result.append(eventData)
+        return result
 
     except:
         print("Tentative de scrapping sur une URL inexistante")
     finally:
         return result
+
+def insertEvents(editions):
+
+    #* Saving to a file
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+        f.write(str(editions))
+
+    # Ajout des index pour chaque event
+    for i in range(0, len(editions)):
+        editions[i]["id"] = i+1
+    print(f"Les index ont été ajoutés pour les {len(editions)} évenements")
+    insertData("INSERT INTO event(name, sport, id) VALUES(%s, %s, %s)", dictToSequence(editions))
+    print(f"Les données ont été insérées en base.")
 
 
 def run():
@@ -74,7 +89,20 @@ def run():
     # Je vais pouvoir boucler sur chaque editions et exécuter la fonction insertEvents dans la boucle
     for i in range(0, len(edtionsInUrl) -1):
         for j in range(0, len(sportsInUrl) -1):
-            eventList.append(data for data in getEvents(edtionsInUrl[i], sportsInUrl[j]))
+            # eventList.append(data for data in getEvents(edtionsInUrl[i], sportsInUrl[j]))
+            events = getEvents(edtionsInUrl[i], sportsInUrl[j])
+
+            if len(events) != 0:
+                for data in events:
+                    if data not in eventList:
+                        eventList.append(data)
+
             time.sleep(random.uniform(TIME_TO_SLEEP/2, TIME_TO_SLEEP*1.5))
-        print(eventList)
+
+            print(f"{j+1} sport sur {len(sportsInUrl)} scrappé pour cette édition >> {((j+1)/len(sportsInUrl))*100}%")
+        print(f"{i+1} éditions sur {len(edtionsInUrl)} scrappé >> {((i+1)/len(edtionsInUrl))*100}%")
+    
+    insertEvents(eventList)
+
+
 run()
