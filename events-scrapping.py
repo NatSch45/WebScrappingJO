7f0930930fc5d858f0e2fec0d47b7fdd42d0022f
t.bourmaud <tao.bourmaud@ynov.com>
2023-05-02 11:13:26 +0200

    get events successfull

--- events-scrapping.py
+++ events-scrapping.py
@@ -8,7 +8,7 @@ import random
 from scrapping import *
 
 #* Constants
-OUTPUT_FILE = './output.json'
+OUTPUT_FILE = './events.json'
 TIME_TO_SLEEP = 0.5
 
 # index = 1
@@ -68,11 +68,6 @@ def getEvents(edition, sport):
         return result
 
 def insertEvents(editions):
-
-    #* Saving to a file
-    with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
-        f.write(str(editions))
-
     # Ajout des index pour chaque event
     for i in range(0, len(editions)):
         editions[i]["id"] = i+1
@@ -80,29 +75,48 @@ def insertEvents(editions):
     insertData("INSERT INTO event(name, sport, id) VALUES(%s, %s, %s)", dictToSequence(editions))
     print(f"Les données ont été insérées en base.")
 
+def readJSON():
+    jsonFile = open(OUTPUT_FILE, "r", encoding="utf-8")
+    events = json.load(jsonFile)
+    return events
+
 
 def run():
     eventList = []
 
-    edtionsInUrl = getEditionsForURL()
-    sportsInUrl = getSportsForURL()
-    # Je vais pouvoir boucler sur chaque editions et exécuter la fonction insertEvents dans la boucle
-    for i in range(0, len(edtionsInUrl) -1):
-        for j in range(0, len(sportsInUrl) -1):
-            # eventList.append(data for data in getEvents(edtionsInUrl[i], sportsInUrl[j]))
-            events = getEvents(edtionsInUrl[i], sportsInUrl[j])
+    try:
+        events = readJSON()
+        if len(events) > 0:
+            for i in range(0, len(events) -1):
+                if events[i]["sport"] == "discipline-canoe-slalom":
+                    events[i]["sport"] = "discipline-canoe-kayak-slalom"
+                if events[i]["sport"] == "discipline-trampoline":
+                    events[i]["sport"] = "discipline-trampoline-gymnastics"
+                eventList.append(events[i])
+        else:
+            edtionsInUrl = getEditionsForURL()
+            sportsInUrl = getSportsForURL()
+            # Je vais pouvoir boucler sur chaque editions et exécuter la fonction insertEvents dans la boucle
+            for i in range(0, len(edtionsInUrl) -1):
+                for j in range(0, len(sportsInUrl) -1):
+                    # eventList.append(data for data in getEvents(edtionsInUrl[i], sportsInUrl[j]))
+                    events = getEvents(edtionsInUrl[i], sportsInUrl[j])
 
-            if len(events) != 0:
-                for data in events:
-                    if data not in eventList:
-                        eventList.append(data)
+                    if len(events) != 0:
+                        for data in events:
+                            if data not in eventList:
+                                eventList.append(data)
 
-            time.sleep(random.uniform(TIME_TO_SLEEP/2, TIME_TO_SLEEP*1.5))
+                    time.sleep(random.uniform(TIME_TO_SLEEP/2, TIME_TO_SLEEP*1.5))
 
-            print(f"{j+1} sport sur {len(sportsInUrl)} scrappé pour cette édition >> {((j+1)/len(sportsInUrl))*100}%")
-        print(f"{i+1} éditions sur {len(edtionsInUrl)} scrappé >> {((i+1)/len(edtionsInUrl))*100}%")
-    
-    insertEvents(eventList)
-
+                    print(f"{j+1} sport sur {len(sportsInUrl)} scrappé pour cette édition >> {((j+1)/len(sportsInUrl))*100}%")
+                print(f"{i+1} éditions sur {len(edtionsInUrl)} scrappé >> {((i+1)/len(edtionsInUrl))*100}%")
+                #* Saving to a file
+            with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+                json.dump(eventList, f)
+    except:
+        print("Un problème lors de l'insertion des données s'est produit")
+    finally:
+        insertEvents(eventList)
 
 run()
